8d8434d26996a4ec95b38ae475e29a23029acee7
fahmanfaisal <fahmanchy@gmail.com>
2026-02-14 23:57:06 +0600

    cat-updated

--- resources/views/components/tables/basic-tables/categories-table.blade.php
+++ resources/views/components/tables/basic-tables/categories-table.blade.php
@@ -33,16 +33,16 @@
     }
 }">
     
-    <div class="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
+    <div class="overflow-hidden">
         <div class="max-w-full overflow-x-auto custom-scrollbar">
             
-            <table class="w-full min-w-[600px]">
+            <table class="w-full min-w-full">
                 
                 <thead>
                     <tr class="border-b border-gray-100 dark:border-gray-800">
                         <th class="px-5 py-3 text-left sm:px-6">Image</th>
                         <th class="px-5 py-3 text-left sm:px-6">Status</th>
-                        <th class="px-5 py-3 text-left sm:px-6">Action</th>
+                        <th class="px-5 py-3 text-right sm:px-6">Action</th>
                     </tr>
                 </thead>
 
@@ -69,8 +69,8 @@
                             </td>
 
                             <!-- Actions -->
-                            <td class="px-5 py-4 sm:px-6">
-                                <div class="flex gap-3">
+                            <td class="px-5 py-4 sm:px-6 w-[40%]">
+                                <div class="flex gap-3 justify-end">
 
                                     <!-- Edit -->
                                     <button @click="openEdit(category)"
@@ -87,6 +87,15 @@
                                         <path d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" stroke="currentColor" stroke-width="1.5"/>
                                         </svg>
                                         Disabled 
+                                    </button>
+
+                                    <button @click="openDelete(category)"
+                                        class="flex gap-2 items-center justify-center w-32 h-12 rounded-lg border border-gray-300 hover:bg-red-50 hover:text-red-600 transition">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                        <path d="M21.1303 9.8531C22.2899 11.0732 22.2899 12.9268 21.1303 14.1469C19.1745 16.2047 15.8155 19 12 19C8.18448 19 4.82549 16.2047 2.86971 14.1469C1.7101 12.9268 1.7101 11.0732 2.86971 9.8531C4.82549 7.79533 8.18448 5 12 5C15.8155 5 19.1745 7.79533 21.1303 9.8531Z" stroke="currentColor" stroke-width="1.5"/>
+                                        <path d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" stroke="currentColor" stroke-width="1.5"/>
+                                        </svg>
+                                        delete 
                                     </button>
 
                                 </div>
@@ -153,6 +162,8 @@
                     </div>
                 </div>
             </template>
+
+
 
         </div>
     </div>
--- resources/views/pages/admin/categories/categories.blade.php
+++ resources/views/pages/admin/categories/categories.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <x-common.page-breadcrumb pageTitle="Categories" />
 
-    <x-common.component-card title="Categories">
+    <x-common.component-card title="Create Categories">
 
         {{-- Header Button --}}
         <x-slot name="action">
@@ -51,7 +51,7 @@
 
             <form class="flex flex-col">
 
-                <div class="custom-scrollbar h-[458px] overflow-y-auto p-2">
+                <div class="custom-scrollbar h-fit-content overflow-y-auto p-2">
 
                     <div class="mb-4">
                         <label for="categoryName"
@@ -66,7 +66,57 @@
                             class="dark:bg-dark-900 shadow-theme-xs focus:border-pink-50 focus:ring-gray-500/10 dark:focus:border-gray-800 h-12 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30">
                     </div>
 
-                    <x-form.form-elements.dropzone />
+                    <div class="mb-4">
+                        <label for="categoryName"
+                            class="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-400">
+                            Category Slug
+                        </label>
+
+                        <input type="text"
+                            id="categorySlug"
+                            name="categorySlug"
+                            placeholder="Enter category slug"
+                            class="dark:bg-dark-900 shadow-theme-xs focus:border-pink-50 focus:ring-gray-500/10 dark:focus:border-gray-800 h-12 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30">
+                    </div>
+
+                    <div>
+                        <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                            Upload Icon
+                        </label>
+                        <input type="file"
+                            class="focus:border-ring-brand-300 shadow-theme-xs focus:file:ring-brand-300 h-11 w-full overflow-hidden rounded-lg border border-gray-300 bg-transparent text-sm text-gray-500 transition-colors file:mr-5 file:border-collapse file:cursor-pointer file:rounded-l-lg file:border-0 file:border-r file:border-solid file:border-gray-200 file:bg-gray-50 file:py-3 file:pr-3 file:pl-3.5 file:text-sm file:text-gray-700 placeholder:text-gray-400 hover:file:bg-gray-100 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-gray-400 dark:text-white/90 dark:file:border-gray-800 dark:file:bg-white/[0.03] dark:file:text-gray-400 dark:placeholder:text-gray-400" />
+                    </div>
+
+                    <div>
+                        <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400 mt-4">
+                            Select Input
+                        </label>
+                        <div x-data="{ isOptionSelected: false }" class="relative z-20 bg-transparent">
+                            <select
+                                class="dark:bg-dark-900 shadow-theme-xs focus:border-pink-50 focus:ring-gray-500/10 dark:focus:border-gray-800 h-11 w-full appearance-none rounded-lg border border-gray-300 bg-transparent bg-none px-4 py-2.5 pr-11 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
+                                :class="isOptionSelected && 'text-gray-800 dark:text-white/90'" @change="isOptionSelected = true">
+                                <option value="" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
+                                    Select Active Option
+                                </option>
+                                <option value="" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
+                                    Enable
+                                </option>
+                                <option value="" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
+                                    Disable
+                                </option>
+                            </select>
+                            <span
+                                class="pointer-events-none absolute top-1/2 right-4 z-30 -translate-y-1/2 text-gray-500 dark:text-gray-400">
+                                <svg class="stroke-current" width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M4.79175 7.396L10.0001 12.6043L15.2084 7.396" stroke="" stroke-width="1.5"
+                                        stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </span>
+                        </div>
+                    </div>
+                    
+
 
                 </div>
 
@@ -81,7 +131,7 @@
 
                     <button 
                         type="button"
-                        class="flex w-full justify-center rounded-lg bg-brand-500 px-4 py-2.5 text-sm font-medium text-white hover:bg-brand-600 sm:w-auto">
+                        class="flex w-full justify-center rounded-lg bg-[#1A1A1A] hover:bg-[#ff84a3] px-4 py-2.5 text-sm font-medium text-white sm:w-auto">
                         Save Changes
                     </button>
 
