3d1f346e709723d587eca28001e48e56c9127419
fahmanfaisal <fahmanchy@gmail.com>
2026-02-15 00:20:36 +0600

    ui-update

--- resources/views/pages/admin/categories/categories.blade.php
+++ resources/views/pages/admin/categories/categories.blade.php
@@ -3,10 +3,28 @@
 @section('content')
     <x-common.page-breadcrumb pageTitle="Categories" />
 
-    <x-common.component-card title="Create Categories">
+    <div class="flex flex-col gap-4 justify-between border rounded-lg border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-white/[0.03]">
+        <div class="flex flex-row justify-end gap-3">
+            <!-- Search Bar (desktop only) -->
+            <div class="hidden xl:block">
+                <form>
+                    <div class="relative">
+                        <span class="absolute -translate-y-1/2 pointer-events-none left-4 top-1/2">
+                            <!-- Search Icon -->
+                            <svg class="fill-gray-500 dark:fill-gray-400" width="20" height="20"
+                                viewBox="0 0 20 20" fill="none">
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M3.04175 9.37363C3.04175 5.87693 5.87711 3.04199 9.37508 3.04199C12.8731 3.04199 15.7084 5.87693 15.7084 9.37363C15.7084 12.8703 12.8731 15.7053 9.37508 15.7053C5.87711 15.7053 3.04175 12.8703 3.04175 9.37363ZM9.37508 1.54199C5.04902 1.54199 1.54175 5.04817 1.54175 9.37363C1.54175 13.6991 5.04902 17.2053 9.37508 17.2053C11.2674 17.2053 13.003 16.5344 14.357 15.4176L17.177 18.238C17.4699 18.5309 17.9448 18.5309 18.2377 18.238C18.5306 17.9451 18.5306 17.4703 18.2377 17.1774L15.418 14.3573C16.5365 13.0033 17.2084 11.2669 17.2084 9.37363C17.2084 5.04817 13.7011 1.54199 9.37508 1.54199Z"
+                                    fill="" />
+                            </svg>
+                        </span>
+                        <input type="text" placeholder="Search Category.."
+                            class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-200 bg-transparent py-2.5 pl-12 pr-14 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-gray-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-800 dark:bg-white/3 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-gray-800 xl:w-[430px]" />
+                        
+                    </div>
+                </form>
+            </div>
 
-        {{-- Header Button --}}
-        <x-slot name="action">
             <button type="button"
                 @click="$dispatch('open-profile-info-modal')"
                 class="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium border border-gray-300 rounded-lg hover:bg-gray-100 transition dark:hover:bg-transparent add-new-btn">
@@ -25,11 +43,11 @@
 
                 <span>Add New</span>
             </button>
-        </x-slot>
+        </div>
 
+        <!-- Categories Table -->
         <x-tables.basic-tables.categories-table />
-
-    </x-common.component-card>
+    </div>
 
 
     <!-- Add Category Modal -->
@@ -93,9 +111,9 @@
                         </label>
                         <div x-data="{ isOptionSelected: false }" class="relative z-20 bg-transparent">
                             <select
-                                class="dark:bg-dark-900 shadow-theme-xs focus:border-pink-50 focus:ring-gray-500/10 dark:focus:border-gray-800 h-11 w-full appearance-none rounded-lg border border-gray-300 bg-transparent bg-none px-4 py-2.5 pr-11 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
+                                class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-50 focus:ring-gray-500/10 dark:focus:border-gray-800 h-11 w-full appearance-none rounded-lg border border-gray-300 bg-transparent bg-none px-4 py-2.5 pr-11 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
                                 :class="isOptionSelected && 'text-gray-800 dark:text-white/90'" @change="isOptionSelected = true">
-                                <option value="" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
+                                <option value="" class="text-gray-700 dark:bg-gray-900 dark:text-white-400">
                                     Select Active Option
                                 </option>
                                 <option value="" class="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
